7940e331c876bcd3d8eb9282f0e194bdfc3c86c4
shardik95 <shardik95@ccs.neu.edu>
2018-09-29 23:24:31 -0400

    Setting up project with initial HelloWorld

---
+++ server/index.js
@@ -0,0 +1,10 @@
+var express = require("express")
+var app = express()
+
+app.get("/",function (req,res) {
+    res.send("Hello World")
+})
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
